3840d0f32375a1eb6082e902d049b4e3c19546ef
mhuss044D430 <mhuss044@gmail.com>
2014-12-18 15:06:45 -0500

    first commit

---
+++ pywebchat.py
@@ -0,0 +1,4 @@
+# make a server, 2 people connect under a room
+# store info into plain text files
+# get around data persistency prop by make two files for each user with timestamps for each entered thing
+# third file for overall chat?
